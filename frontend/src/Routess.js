1426609be1a574ebe535a9ccbf47c0d01977917c
Nishant <rd5762331@g,ail.com>
2025-02-24 10:52:50 +0530

    Milestone-11

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -3,15 +3,13 @@ import SignUpPage from "../src/pages/SignUpPage.jsx"
 import ActivationPage from "../src/pages/ActivationPage.jsx";
 import Home from "../src/pages/Home.jsx"
 import CreateProduct from "../src/pages/createProduct.jsx"
-import MyProducts from "../src/pages/myProducts.jsx";
 export
 {
     LoginPage,
     SignUpPage,
     ActivationPage,
     Home,
-    CreateProduct,
-    MyProducts
+    CreateProduct
 
 }
 
